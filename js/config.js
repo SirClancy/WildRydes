a1c956da685c37bc14406499fcbd7903fae8686e
SirClancy <clancymudaki@gmail.com>
2024-03-27 11:17:35 +0300

    Initial aws Template for serveless design

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_1hvBFBlk5', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5cjq6u6qrdd9712s4ljhnv4gg9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://wpkqfb2s97.execute-api.us-west-2.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
